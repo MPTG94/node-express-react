aead9fc4a2697aa67391f99ecbda16dceae68be7
Mor Paz <1912.mor@gmail.com>
2016-10-30 18:36:28 +0200

    Added classes to spa page

--- reactspa.js
+++ reactspa.js
@@ -1,5 +1,5 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 var contacts = [
     {key: 1, name: 'James K Nelson', email: 'james@jamesknelson.com',
@@ -8,18 +8,35 @@ var contacts = [
     {key: 3, name: 'Joe'},
 ];
 
+var ContactItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string
+  },
+
+  render: function() {
+    return (
+      React.createElement('li', {key: this.props.key, className: 'Contact'},
+        React.createElement('h2', {className: 'Contact-name'}, this.props.name),
+        React.createElement('a', {href: 'mailto:' + this.props.email},
+        this.props.email),
+        React.createElement('div', {className: 'Contact-Description'},
+        this.props.description)
+      )
+    );
+  }
+});
+
+var listElement = contacts
+  .filter(function(contact) { return contact.email; })
+  .map(function(contact) {
+    return React.createElement(ContactItem, contact);
+  });
+
 var rootElement =
   React.createElement('div', {},
     React.createElement('h1', {}, 'Contacts'),
-    React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'James Nelson'),
-        React.createElement('a', {href: 'mailto:james@jamesknelson.com'},
-      'james@jamesknelson.com')
-    ),
-    React.createElement('li', {},
-      React.createElement('h2', {}, 'Joe Citizen'),
-      React.createElement('a', {href: 'mailto:joe@example.com'},
-      'joe@example.com'))));
+    React.createElement('ul', {}, listElement));
 
 ReactDOM.render(rootElement, document.getElementById('react-app'));
